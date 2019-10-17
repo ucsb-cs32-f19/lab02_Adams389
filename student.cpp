2cda2531e09e02d814b17d02871b07e0d0cea250
Jiayu Chen <jiayuchen@umail.ucsb.edu>
2019-10-17 09:56:28 -0700

    lab02

---
+++ student.cpp
@@ -0,0 +1,66 @@
+#include "student.h"
+#include <string>
+#include <cstring>
+#include <sstream>
+Student::Student(const char * const name, int perm) {
+  this->setName(name);
+  this->setPerm(perm);
+}
+
+int Student::getPerm() const {
+  return this->perm;
+}
+
+const char * const Student::getName() const {
+  return this->name;
+}
+
+void Student::setPerm(const int permNumber) {
+    this->perm = permNumber;
+}
+
+void Student::setName(const char * const name) {
+  this->name = new char[strlen(name)+1];
+  strcpy(this->name,name);
+}
+
+
+Student::Student(const Student &orig) {
+  this->setName(orig.name);
+  this->setPerm(orig.perm);
+}
+
+Student::~Student() {
+    delete name;
+}
+
+Student & Student::operator=(const Student &right) {
+
+  // The next two lines are standard, and you should keep them.
+  // They avoid problems with self-assignment where you might free up 
+  // memory before you copy from it.  (e.g. x = x)
+
+  if (&right == this) 
+    return (*this);
+
+  // TODO... Here is where there is code missing that you need to 
+  // fill in...
+    this->setPerm(right.perm);
+    delete this->name;
+    this->setName(right.name);
+
+  // KEEP THE CODE BELOW THIS LINE
+  // Overloaded = should end with this line, despite what the textbook says.
+  return (*this); 
+
+}
+
+std::string Student::toString() const {
+  std::ostringstream oss;
+
+  oss<<"["
+     << this->getName()<<","
+     << this->getPerm()<<"]";
+  return oss.str();
+}
+
