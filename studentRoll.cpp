8faab16a890c2c177b7c5c9edcb5d1287da84141
Jiayu Chen <1132655514@qq.com>
2019-10-21 11:27:40 -0700

    Update studentRoll.cpp

--- studentRoll.cpp
+++ studentRoll.cpp
@@ -1,28 +1,33 @@
 #include <string>
 #include "studentRoll.h"
+using namespace std;
+#include <iostream>
 
 StudentRoll::StudentRoll() {
   head = tail = NULL;
 }
 
-void StudentRoll::insertAtTail(const Student &s) {
-  tail->s = s;
-  tail->next = NULL;
-  tail = tail->nest;
+void StudentRoll::insertAtTail(const Student &stu) {
+  Node *n = new Node;
+  tail->next = n;
+  tail = n;
+  n->next = NULL;
+  n->s->setPerm(stu.getPerm());
+  n->s->setName( stu.getName());
 }
 
 std::string StudentRoll::toString() const {
   for(Node* i = head; i != NULL; i = i->next){
-    i->s.toString();
+    i->s->toString();
     if(i->next != NULL){
-      sdt::cout<<",";
+      cout<<",";
     }
   }
 }
 
 StudentRoll::StudentRoll(const StudentRoll &orig) {
   Node* iter = this->head;
-  iter->head = orig->head;
+  head = orig.head;
   for (Node*i = orig.head; i != NULL; i = i->next){
     iter->next = i->next;
     iter = iter->next;
@@ -32,26 +37,26 @@ StudentRoll::StudentRoll(const StudentRoll &orig) {
 
 StudentRoll::~StudentRoll() {
   Node* cur = head;
-  while(cur!=NULL)(
-    Node*next = cur->next;
+  while(cur!=NULL){
+    Node*nexte = cur->next;
     delete cur;
-    cur = next;
+    cur = nexte;
   }
 }
 
 StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
   // The next two lines are standard, and you should keep them.
-  // They avoid problems with self-assignment where you might free up 
+  // They avoid problems with self-assignment where you might free up
   // memory before you copy from it.  (e.g. x = x)
 
-  if (&right == this) 
+  if (&right == this)
     return (*this);
 
-  // TODO... Here is where there is code missing that you need to 
+  // TODO... Here is where there is code missing that you need to
   // fill in...
   delete this;
   Node* iter = this->head;
-  Node*i = right->head;
+  Node*i = right.head;
   while(i != NULL){
     iter->s = i->s;
     iter = iter->next;
@@ -61,8 +66,8 @@ StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
 
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
-  return (*this); 
-  
+  return (*this);
+
 }
 
 
